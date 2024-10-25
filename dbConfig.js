8d81626f01e255fd016212df16a5e46dd9a0106a
Sudeep  Shivashettar <109416576+ShettyBro@users.noreply.github.com>
2024-10-25 11:54:29 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,8 +1,9 @@
+
 const dbConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
+  user: String(process.env.DB_USER),
+  password: String(process.env.DB_PASSWORD),
+  server: String(process.env.DB_SERVER),
+  database: String(process.env.DB_NAME),
   options: {
     encrypt: true,
     enableArithAbort: true
