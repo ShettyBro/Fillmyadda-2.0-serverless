29f80f48b5699d532b99a30ac72502ae18ce1a27
Sudeep  Shivashettar <109416576+ShettyBro@users.noreply.github.com>
2024-10-25 12:02:12 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,10 @@
-
 const dbConfig = {
-  user: String(process.env.DB_USER),
-  password: String(process.env.DB_PASSWORD),
-  server: String(process.env.DB_SERVER),
-  database: String(process.env.DB_NAME),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
   options: {
-    encrypt: true,
-    enableArithAbort: true
+    encrypt: true,           // Important for Azure SQL
+    enableArithAbort: true    // Helps prevent connection issues with some SQL setups
   }
 };
