85de1a70bcf877e945231d88f38db7e12485739f
Sudeep  Shivashettar <sudeepshivashettar@gmail.com>
2024-10-25 13:17:02 +0530

    done

--- dbConfig.js
+++ dbConfig.js
@@ -9,3 +9,10 @@ const dbConfig = {
     connectionTimeout: 30000   // Set to 30 seconds
   }
 };
+const poolPromise = new sql.ConnectionPool(config)
+    .connect()
+    .then(pool => {
+        console.log('Connected to SQL Server');
+        return pool;
+    })
+    .catch(err => console.log('Database connection failed: ', err));
