1b43429d41ab506b26315a833a9497d1df3be6f2
Sudeep  Shivashettar <sudeepshivashettar@gmail.com>
2025-03-18 11:47:00 +0530

    t5

--- public/js/script.js
+++ public/js/script.js
@@ -516,6 +516,7 @@ function selectMovie(movieId) {
 
 
 
+
 // send OTP
 document.getElementById('SendLink').addEventListener('click', async function (e) {
     e.preventDefault();
@@ -558,7 +559,51 @@ document.getElementById('SendLink').addEventListener('click', async function (e)
 
 
 
-/
+//otp veriifcation function
+document.getElementById('validate').addEventListener('click', async function (e) {
+    e.preventDefault();
+    const button = e.target;
+    const otp = document.getElementById('otpInput').value.trim();
+
+    const storedEmail = localStorage.getItem("userEmail");
+
+    // Disable button and show loading text
+    button.disabled = true;
+    button.innerText = 'Verifying OTP...';
+
+    
+
+    try {
+        const response = await fetch("https://filmyadda.sudeepbro.me/.netlify/functions/otpver?action=verifyOTP", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ storedEmail, otp })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem("resetToken", data.token); // Store JWT token
+            showModal("✅ OTP verified successfully! Redirecting...");
+
+            setTimeout(() => {
+                window.location.href = "reset.html"; // Redirect to reset password page
+            }, 2000);
+        } else {
+            showModal(`❌ ${data.message}`);
+        }
+    } catch (error) {
+        console.error("OTP verification error:", error);
+            showModal("❌ Something went wrong. Please try again.");
+    } finally {
+        // Re-enable button
+        button.disabled = false;
+        button.innerText = 'Send Link';
+    }
+});
+
+
+
 
 //reset password function
 document.getElementById("resetForm").addEventListener("submit", async function (e) {
